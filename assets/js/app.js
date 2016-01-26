5e6bd12fafa139d57ae8a182b7401a11d40a1392
anqqa <anttiq@gmail.com>
2016-01-26 11:53:07 +0200

    Login/Register form and error styling

---
+++ assets/js/app.js
@@ -0,0 +1,47 @@
+(function($) {
+    "use strict";
+
+    // Handle Ajax error messages
+    $(window).on('ajaxErrorMessage', function(event, message) {
+        $('#alerts').html($('<li>').addClass('alert alert--error').html(message));
+
+        event.preventDefault();
+    });
+
+    $(window).on('ajaxInvalidField', function(event, field, fieldName, messages, firstInvalid) {
+
+        // Clear old errors
+        if (firstInvalid) {
+            $('.input--error')
+                .removeClass('input--error')
+                .find('.input__message')
+                    .remove();
+
+            $('.input__field--error').removeClass('input__field--error');
+            $('.input__message--error').remove();
+        }
+
+        var $field = $(field);
+        var $error = $('<p>')
+            .addClass('input__message')
+            .html(messages.join('<br>'));
+
+
+        if ($field.hasClass('input__field') && $field.parent('.input')) {
+            $field.parent('.input').addClass('input--error');
+        }
+        else {
+            $field.addClass('input__field input__field--error');
+            $error.addClass('input__message--error');
+        }
+
+        $field.after($error);
+
+        event.preventDefault();
+    });
+
+    $(document).ready(function() {
+
+    });
+
+}(window.jQuery));
